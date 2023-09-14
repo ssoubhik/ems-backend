2fa04bb6ab3f3b15a5374d30393ada20de7e5c98
Soubhik Sarkhel <soubhik@annantasourcellc.onmicrosoft.com>
2023-09-14 19:44:39 +0530

    feat: added PORT 8080

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const http = require('http');
 // Import app.js
 const app = require('./app');
 
+
 // Initialize Port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Initialize Server
 const server = http.createServer(app);
 
 // Start Server
-server.listen(port);
+server.listen(port, () => console.log(`Listening on port ${port}`));
